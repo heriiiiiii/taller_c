c44b57b722ce715462443f59259387e247d1dd1f
heriiiiiii <vvh0033477@est.univalle.edu>
2023-05-12 11:28:21 -0400

    segunda version

--- Program.cs
+++ Program.cs
@@ -1,10 +1,14 @@
-﻿namespace git
+﻿namespace taller_git01
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            int n1, n2, result;
+            n1 = 23;
+            n2 = 45;
+            result = n1 + n2;
+            Console.WriteLine("El resultado es: "+result);
         }
     }
 }
